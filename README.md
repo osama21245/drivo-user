d37d287219f9202f457500ec27cda5782bafbfe2
osama gamil <108416526+osama21245@users.noreply.github.com>
2025-05-12 14:28:51 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ur_turn_user_app
+ 
+# drivo_user_app
+# drivo_user_app
---
+++ android/app/src/main/kotlin/com/devu/hwilo_user/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.drivo.hwilo_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
